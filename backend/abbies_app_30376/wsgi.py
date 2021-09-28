76aba30216b48cc184e853fcbe30b609e02fd539
Crowdbotics <team@crowdbotics.com>
2021-09-28 20:24:55 +0000

    Update models

--- backend/abbies_app_30376/wsgi.py
+++ backend/abbies_app_30376/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abbies_app_30376.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "abbies_app_30376.settings")
 
 application = get_wsgi_application()
